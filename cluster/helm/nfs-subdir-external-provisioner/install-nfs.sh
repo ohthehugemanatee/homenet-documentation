5673be4019f86127938f1604b6fc9ac5f91bca5f
Campbell Vertesi <campbell@vertesi.com>
2021-08-16 21:49:50 +0200

    Add nfs default storageclass

---
+++ cluster/helm/nfs-subdir-external-provisioner/install-nfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --set nfs.server=192.168.1.50 \
+  --set nfs.path=/mnt/storage/.docker/kubernetes \
+  --namespace kube-system
+
+# Set nfs as the default storageClass
+k patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+k patch storageclass nfs-client -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+# All done! Show storageclasses
+k get storageclass
+
