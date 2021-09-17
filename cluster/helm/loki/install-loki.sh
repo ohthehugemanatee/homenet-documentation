b0994dcf4e9b099a0e8ca19320fb6eef3e7a8edb
Campbell Vertesi <campbell@vertesi.com>
2021-09-17 14:35:05 +0200

    Add loki

---
+++ cluster/helm/loki/install-loki.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm upgrade --install loki grafana/loki-stack  --set grafana.enabled=true,prometheus.enabled=true
+
