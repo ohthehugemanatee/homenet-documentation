d7f73e45b8089b0d0962df226b48d0083aa2dea1
Campbell Vertesi <campbell@vertesi.com>
2021-11-10 10:04:59 +0100

    Update nfs server ip address

--- cluster/helm/nfs-subdir-external-provisioner/install-nfs.sh
+++ cluster/helm/nfs-subdir-external-provisioner/install-nfs.sh
@@ -2,7 +2,7 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-  --set nfs.server=192.168.1.50 \
+  --set nfs.server=10.10.10.50 \
   --set nfs.path=/mnt/storage/.docker/kubernetes \
   --namespace kube-system
 
