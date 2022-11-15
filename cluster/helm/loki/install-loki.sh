943dec081a061e44d1b61599cee0bf629924bea3
Campbell Vertesi <campbell@vertesi.com>
2022-11-15 11:25:26 +0100

    Update loki installation

--- cluster/helm/loki/install-loki.sh
+++ cluster/helm/loki/install-loki.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm upgrade --install -n loki loki grafana/loki-stack  --set grafana.enabled=true,prometheus.enabled=true,loki.persistence.enabled=true,loki.persistence.size=5Gi
+helm upgrade --install -n loki loki grafana/loki-stack  -f values.yaml
 # nb: if the helm chart is still running kube-state-metrics 1.x, you need to edit the Deployment to nodeSelector amd64
-kubectl apply -f ../../services/grafana.yaml
 echo -n "password: "
 kubectl get secret --namespace loki loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
