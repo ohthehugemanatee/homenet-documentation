e61abbbd665fc0b4aa85f6974f443175b166feb1
Campbell Vertesi <campbell@vertesi.com>
2024-07-10 14:00:28 +0200

    Configure fixed LoadBalancerIP for plex and traefik

--- cluster/metallb/install-metallb.sh
+++ cluster/metallb/install-metallb.sh
@@ -6,3 +6,5 @@
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
 
 kubectl apply -f l2-config.yaml
+
+kubectl patch -n kube-system traefik -p '{"spec":{"LoadBalancerIP":"10.10.11.2"}}'
