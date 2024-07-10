b141a748e83a86fe26fd774a3be597b88df7687d
Campbell Vertesi <campbell@vertesi.com>
2024-07-10 12:48:38 +0200

    Add metallb

---
+++ cluster/metallb/README.md
@@ -0,0 +1,3 @@
+# MetalLB configuration
+
+Per [MetalLB docs] k3s control plane nodes have to be started with `--disable=servicelb`. This change is already reflected in the ansible playbook in this repo.
---
+++ cluster/metallb/install-metallb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install metallb.
+
+# Get current version from metallb docs: https://metallb.io/installation/
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+
+kubectl apply -f l2-config.yaml
