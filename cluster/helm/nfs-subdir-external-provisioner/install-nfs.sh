da402161d30df9c6689203abcfac005334656454
Campbell Vertesi <campbell@vertesi.com>
2026-06-13 22:29:44 +0200

    Fix broken nfs-provisioner path (#75)

--- cluster/helm/nfs-subdir-external-provisioner/install-nfs.sh
+++ cluster/helm/nfs-subdir-external-provisioner/install-nfs.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+helm upgrade nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
   --set nfs.server=shoebox.vert \
-  --set nfs.path=/export/storage/configs/kubernetes \
+  --set nfs.path=/export/configs/kubernetes \
   --namespace kube-system
-
-# Set nfs as the default storageClass
-k patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-
-k patch storageclass nfs-client -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 # All done! Show storageclasses
 k get storageclass
